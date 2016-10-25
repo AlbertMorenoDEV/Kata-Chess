39b60951927bc506e7ca83bb5e803a94f867e50a
Albert Moreno <albert.moreno.dev@gmail.com>
2016-10-26 00:32:15 +0200

    ChessTest

--- tests/ChessTest.php
+++ tests/ChessTest.php
@@ -4,5 +4,22 @@ use PHPUnit\Framework\TestCase;
 
 final class ChessTest extends TestCase
 {
+    private $chessboard;
 
+    protected function setUp()
+    {
+        $this->chessboard = new Chessboard();
+    }
+
+    /** @test */
+    public function mustBeOneChessboard()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function mustBeTwoPlayers()
+    {
+        $this->assertTrue(true);
+    }
 }
