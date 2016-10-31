6f6868a56de915a6f6ae326d3c96a7693b004775
Albert Moreno <albert.moreno.dev@gmail.com>
2016-11-01 00:13:43 +0100

    Test updated

--- tests/ChessTest.php
+++ tests/ChessTest.php
@@ -1,28 +1,30 @@
 <?php
 
 use AlbertMorenoDEV\KataChess\Chessboard;
+use AlbertMorenoDEV\KataChess\Game;
+use AlbertMorenoDEV\KataChess\Player;
 use PHPUnit\Framework\TestCase;
 
 final class ChessTest extends TestCase
 {
-    /** @var $chessboard Chessboard */
-    private $chessboard;
+    /** @var $game Game */
+    private $game;
 
     protected function setUp()
     {
-        $this->chessboard = new Chessboard();
+        $this->game = new Game(new Chessboard(), new Player('A'), new Player('B'));
     }
 
     /** @test */
     public function mustBeOneChessboard()
     {
-        $this->assertInstanceOf(Chessboard::class, $this->chessboard);
+        $this->assertInstanceOf(Chessboard::class, $this->game->getChessboard());
     }
 
     /** @test */
     public function eightByEightSquares()
     {
-        $squares = $this->chessboard->getSquares();
+        $squares = $this->game->getChessboard()->getSquares();
         $this->assertTrue(isset($squares['a']));
         $this->assertTrue(isset($squares['a'][1]));
         $this->assertTrue(isset($squares['h']));
@@ -32,6 +34,7 @@ final class ChessTest extends TestCase
     /** @test */
     public function mustBeTwoPlayers()
     {
-        // ToDo
+        $this->assertInstanceOf(Player::class, $this->game->getPlayerA());
+        $this->assertInstanceOf(Player::class, $this->game->getPlayerB());
     }
 }
