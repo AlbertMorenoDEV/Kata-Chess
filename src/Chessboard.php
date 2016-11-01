8c8c9c0032adb5cd34812aad4c0fd6f0467d77c3
Albert Moreno <albert.moreno.dev@gmail.com>
2016-11-01 23:39:39 +0100

    Chessboard

--- src/Chessboard.php
+++ src/Chessboard.php
@@ -1,7 +1,9 @@
 <?php
 namespace AlbertMorenoDEV\KataChess;
 
-class Chessboard
+use AlbertMorenoDEV\KataChess\Pieces\iPiece;
+
+class Chessboard implements iChessboard
 {
     private $squares = [];
     
@@ -10,18 +12,44 @@ class Chessboard
         $this->initialize();
     }
 
-    public function getSquares()
+    public function getSquares() : array
     {
         return $this->squares;
     }
 
-    private function initialize()
+    public function setPieceOnSquare($x, $y, iPiece $piece)
+    {
+        $this->getSquare($x, $y)->setPiece($piece);
+    }
+
+    public function getSquare($x, $y) : iSquare
+    {
+        return $this->squares[$x][$y];
+    }
+
+    public function walkAllPositions(callable $callback)
     {
         for ($x = 'a'; $x <= 'h'; $x++) {
-            $this->squares[$x] = [];
             for ($y = 1; $y <= 8; $y++) {
-                $this->squares[$x][$y] = new Square($x, $y);
+                call_user_func($callback, $x, $y);
             }
         }
+    }
+
+    public function walkAllSquares(callable $callback)
+    {
+        $this->walkAllPositions(function ($x, $y) use (&$callback) {
+            call_user_func($callback, $this->getSquare($x, $y));
+        });
+    }
+
+    private function initialize()
+    {
+        $this->walkAllPositions(function ($x, $y) {
+            if (!isset($this->squares[$x])) {
+                $this->squares[$x] = [];
+            }
+            $this->squares[$x][$y] = new Square($x, $y);
+        });
     }
 }
