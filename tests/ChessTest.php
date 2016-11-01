4b712d3418de6d15e94ad643ef285cecdd0e89d7
Albert Moreno <albert.moreno.dev@gmail.com>
2016-11-01 23:41:12 +0100

    Test updated

--- tests/ChessTest.php
+++ tests/ChessTest.php
@@ -2,6 +2,9 @@
 
 use AlbertMorenoDEV\KataChess\Chessboard;
 use AlbertMorenoDEV\KataChess\Game;
+use AlbertMorenoDEV\KataChess\iSquare;
+use AlbertMorenoDEV\KataChess\Pieces\Colors\Black;
+use AlbertMorenoDEV\KataChess\Pieces\Colors\White;
 use AlbertMorenoDEV\KataChess\Player;
 use PHPUnit\Framework\TestCase;
 
@@ -12,7 +15,7 @@ final class ChessTest extends TestCase
 
     protected function setUp()
     {
-        $this->game = new Game(new Chessboard(), new Player('A'), new Player('B'));
+        $this->game = new Game(new Chessboard(), new Player('A', new White()), new Player('B', new Black()));
     }
 
     /** @test */
@@ -36,5 +39,25 @@ final class ChessTest extends TestCase
     {
         $this->assertInstanceOf(Player::class, $this->game->getPlayerA());
         $this->assertInstanceOf(Player::class, $this->game->getPlayerB());
+    }
+
+    /** @test */
+    public function mustBeEightPiecesOfEachColor()
+    {
+        $whiteTotal = 0;
+        $blackTotal = 0;
+
+        $this->game->getChessboard()->walkAllSquares(function (iSquare $square) use (&$whiteTotal, &$blackTotal) {
+            if ($square->isSet()) {
+                if ($square->getPiece()->getPlayer()->getColor() instanceof White) {
+                    $whiteTotal++;
+                } elseif ($square->getPiece()->getPlayer()->getColor() instanceof Black) {
+                    $blackTotal++;
+                }
+            }
+        });
+
+        $this->assertEquals(16, $whiteTotal);
+        $this->assertEquals(16, $blackTotal);
     }
 }
