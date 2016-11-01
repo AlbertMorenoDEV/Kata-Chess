74d8dd6558ec1bb970eff63d008e0e43595c67c1
Albert Moreno <albert.moreno.dev@gmail.com>
2016-11-01 23:40:54 +0100

    Square

--- src/Square.php
+++ src/Square.php
@@ -1,14 +1,36 @@
 <?php
 namespace AlbertMorenoDEV\KataChess;
 
-class Square
+use AlbertMorenoDEV\KataChess\Pieces\iPiece;
+
+class Square implements iSquare
 {
     private $x;
     private $y;
+    private $piece;
 
     public function __construct($x, $y)
     {
         $this->x = $x;
         $this->y = $y;
+    }
+
+    public function setPiece(iPiece $piece)
+    {
+        if (isset($this->piece)) {
+            throw new \Exception('Square already in use.');
+        }
+
+        $this->piece = $piece;
+    }
+
+    public function isSet(): bool
+    {
+        return (isset($this->piece));
+    }
+
+    public function getPiece(): iPiece
+    {
+        return $this->piece;
     }
 }
