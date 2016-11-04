c1793d38e2903f9de783655235422513e4425c6f
Albert Moreno <albert.moreno.dev@gmail.com>
2016-11-04 23:53:15 +0100

    Value Objects

---
+++ src/ValueObjects/Position.php
@@ -0,0 +1,24 @@
+<?php
+namespace AlbertMorenoDEV\KataChess\ValueObjects;
+
+class Position
+{
+    private $x;
+    private $y;
+
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    public function getY()
+    {
+        return $this->y;
+    }
+}
